b9af3ef65bff91f5e66fd2a8831dff3d567f52de
pochaka <groznykh.timur@yandex.ru>
2019-03-19 11:54:32 +0300

    Chapter 7

---
+++ src/golang-book/chapter7/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	xs := []float64{98, 93, 77, 82, 83}
+	fmt.Println("Average", average(xs))
+	fmt.Println("Add", add(1, 2, 3))
+}
+
+func average(xs []float64) float64 {
+	total := 0.0
+	for _, value := range xs {
+		total += value
+	}
+	return total / float64(len(xs))
+}
+
+func add(args ...int) int {
+	total := 0
+	for _, value := range args {
+		total += value
+	}
+	return total
+}
