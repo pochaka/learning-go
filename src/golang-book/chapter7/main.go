8ba37959a10fd9c6dc7114bce040965e4a5006b1
pochaka <groznykh.timur@yandex.ru>
2019-05-24 23:52:34 +0300

    closure

--- src/golang-book/chapter7/main.go
+++ src/golang-book/chapter7/main.go
@@ -6,6 +6,14 @@ func main() {
 	xs := []float64{98, 93, 77, 82, 83}
 	fmt.Println("Average", average(xs))
 	fmt.Println("Add", add(1, 2, 3))
+	sum := func(x, y int) int {
+		return x + y
+	}
+	fmt.Println(sum(int(xs[1]), int(xs[2])))
+	nextEven := makeEvenGenerator()
+	fmt.Println(nextEven())
+	fmt.Println(nextEven())
+	fmt.Println(nextEven())
 }
 
 func average(xs []float64) float64 {
@@ -22,4 +30,13 @@ func add(args ...int) int {
 		total += value
 	}
 	return total
+}
+
+func makeEvenGenerator() func() uint {
+	i := uint(0)
+	return func() (ret uint) {
+		ret = i
+		i += 2
+		return
+	}
 }
